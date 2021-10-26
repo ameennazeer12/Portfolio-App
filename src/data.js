4085e7c6ac420a95ca6484ac72409df5a6cefa9d
Ameen <ameennazeer12@gmail.com>
2021-10-26 17:59:45 +0530

    Initial Deploy

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import tasbeehpic from "./projectpics/tasbeeh.png";
+import quranpic from "./projectpics/quranpic.png";
+export const projects = [
+
+  {
+    title: "Tasbeeh App",
+    subtitle: "Progressive Web App built using React ",
+    description:
+      "Tasbeeh Counter App to keep track of your Zikr",
+    image: tasbeehpic,
+    link: "https://github.com/ameennazeer12/TasbeehApp.git",
+  },
+
+  {
+    title: "Quran App",
+    subtitle: "Web App built using React and Bootstrap ",
+    description:
+      "API used to fetch Quran Ayah's",
+    image: quranpic,
+    link: "https://github.com/ameennazeer12/QuranApp.git",
+  },
+];
+export const skills = [
+  'ReactJS', 'JavaScript (ES6+)','CSS3','HTML5','Bootstrap','TailwindCSS','Python','C++','SAP ABAP','SQL','Git','PWA',
+]
